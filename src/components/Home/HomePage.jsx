47e21cf7c2244aad88cddc4e351b7854b9357575
Mariana Santos <154370339+MarianaToja@users.noreply.github.com>
2025-02-03 16:42:42 -0300

    Home Page com carrosel

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import './HomePage.css'
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './HomePage.css';
+
+import medico from '../../assets/images/medico.png';
+import azul from '../../assets/images/azul.png';
 
 function HomePage() {
     return (
-        <>
-            <body>
-                <img src="../src/assets/images/medico.jpg" />
-            </body>
-        </>
-    )
+        <div className='carrossel'>
+            <Carousel>
+                <Carousel.Item>
+                    <img src={medico} alt="Imagem um" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={medico} alt="Imagem dois" />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
 }
 
-
-export default HomePage
+export default HomePage;
