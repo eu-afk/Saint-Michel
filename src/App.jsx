ab32ad23a9328ed6f529293fd16bebfbdc374ab3
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-04 14:35:36 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 // import LoginPage from "./components/Login/LoginPage"
 import Footer from "./components/Footer/Footer"
 import NavBar from "./components/Navbar/NavBar"
+import Login from "./Pages/Login/LoginPage"
 import './index.css'
 
 function App() {
 
   return (
     <>
+      
     </>
   )
 }
