373a0d6f1625de57cb260fdb08a02184a924f7cd
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-05 00:41:32 -0300

    continuando a pagina de sobre

--- src/Pages/Sobre/SobrePage.jsx
+++ src/Pages/Sobre/SobrePage.jsx
@@ -14,40 +14,39 @@ export default function SobrePage() {
                 <div className="child-sobre">
 
                     <div className="container-sobre">
-
                         <h1 className='titulo-sobre'>BEM-VINDO AO SAINT-MICHEL</h1>
-                        <h1 className='subTitulo-sobre'>Melhor lugar para
-                            cuidar da sua saúde! </h1>
+                        <h1 className='subTitulo-sobre'>Melhor lugar para</h1>
+                        <h1 className='subTitulo-sobre'>cuidar da sua saúde! </h1>
                     </div>
 
                     <div className='container-listas'>
-                        <ul>
-                            <li>A Passion for Healing</li>
-                            <li>All our best</li>
-                            <li>Always Caring</li>
+                        <ul className='ul-container-listas'>
+                            <li className='li-container-listas'>A Passion for Healing</li>
+                            <li className='li-container-listas'>All our best</li>
+                            <li className='li-container-listas'>Always Caring</li>
                         </ul>
 
-                        <ul>
-                            <li>5-Star Care</li>
-                            <li>Believe in Us</li>
-                            <li>A Legacy of Excellence</li>
+                        <ul className='ul-container-listas'>
+                            <li className='li-container-listas'>5-Star Care</li>
+                            <li className='li-container-listas'>Believe in Us</li>
+                            <li className='li-container-listas'>A Legacy of Excellence</li>
                         </ul>
                     </div>
 
                     <div className="container-texto-sobre">
-
-                        <p className="texto-sobre">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam doloremque quam, dicta eius debitis nulla odio fugiat a illum cupiditate illo iusto porro, laudantium quis error at nisi soluta quia!
-                        </p>
-                        
                         <p className="texto-sobre">
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam doloremque quam, dicta eius debitis nulla odio fugiat a illum cupiditate illo iusto porro, laudantium quis error at nisi soluta quia!
                         </p>
 
+                        <p className="texto-sobre">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam doloremque quam, dicta eius debitis nulla odio fugiat a illum cupiditate illo iusto porro, laudantium quis error at nisi soluta quia!
+                        </p>
                     </div>
                 </div>
 
             </div>
+
+            <img src="../src/img/Banner-Baixo-Sobre.png" alt="" className='Banner-Baixo-Sobre' />
 
             <Footer />
         </>
