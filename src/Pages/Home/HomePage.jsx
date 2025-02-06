bd0bb9fbed4c681f45fcd66c542027a9ebd32874
Mariana Santos <154370339+MarianaToja@users.noreply.github.com>
2025-02-06 16:40:08 -0300

    more update

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -7,12 +7,48 @@ import medicos from '../../img/medicos.png';
 import azul from '../../img/azul.png';
 import Navbar from '../../components/Navbar/NavBar';
 import Footer from '../../components/Footer/Footer';
+import checkup from '../../img/checkup.png';
+import cardiograma from '../../img/cardiograma.png';
+import dna from '../../img/dna.png';
+import bancoSangue from '../../img/bancoSangue.png';
+import grupo from '../../img/grupo.png';
+import cuidados from '../../img/cuidados.png';
+import seta from '../../img/seta.png'
+
+// Importando imagens das especialidades corretamente
+import neurologia from '../../img/neurologia.png';
+import ortopedia from '../../img/ortopedia.png';
+import oncologia from '../../img/oncologia.png';
+import otorrino from '../../img/otorrino.png';
+import oftalmologia from '../../img/oftalmologia.png';
+/* import cardiologia from '../../img/cardiologia.png'; */
+import pneumologia from '../../img/pneumologia.png';
+import renal from '../../img/renal.png';
+/*import gastro from '../../img/gastro.png';
+import urologia from '../../img/urologia.png';
+import dermatologia from '../../img/dermatologia.png';
+import ginecologia from '../../img/ginecologia.png';*/
 
 const services = [
-    { img: '../src/img/checkup.png', style: 'light' },
-    { img: '../src/img/cardiograma.png', style: 'dark' },
-    { img: '../src/img/dna.png', style: 'light' },
-    { img: '../src/img/bancoSangue.png', style: 'light' }
+    { img: checkup, style: 'light' },
+    { img: cardiograma, style: 'dark' },
+    { img: dna, style: 'light' },
+    { img: bancoSangue, style: 'light' }
+];
+
+const especialidades = [
+    { img: neurologia },
+    { img: ortopedia, style: 'dark'  },
+    { img: oncologia },
+    { img: otorrino },
+    {img: oftalmologia},
+    /* { img: cardiologia, nome: "Cardiovascular" }, */
+    { img: pneumologia },
+    { img: renal },
+    /*{ img: gastro, nome: "Gastroenterologia" },
+    { img: urologia, nome: "Urologia" },
+    { img: dermatologia, nome: "Dermatologia" }
+    { img: ginecologia, nome: "Ginecologia" }*/
 ];
 
 const HomePage = () => {
@@ -35,66 +71,69 @@ const HomePage = () => {
                 <h4 className='segundoTitulo'>Um ótimo lugar para receber cuidados</h4>
                 <p className='paragrafo'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat <br />scelerisque tortor ornare ornare. Convallis felis vitae tortor augue. Velit nascetur<br /> proin massa in. Consequat faucibus porttitor enim et.</p>
 
-                <p className='saibaMais'> <a href="">Saiba mais</a>
-                    <img className='seta' src="../src/img/seta.png" />
+                <p className='saibaMais'> <a href="#">Saiba mais</a>
+                    <img className='seta' src={seta} alt="Seta" />
                 </p>
-                <img className='medico' src="../src/img/medicos.png" />
+                <img className='medico' src={medicos} alt="Médicos" />
                 <p className='titulo'>CUIDADO EM QUE VOCÊ PODE ACREDITAR</p>
                 <h4 className='segundoTitulo'>Nossos Serviços</h4>
-
-
-            </div >
-
+            </div>
 
             <div className="services-container">
                 <div className="menu">
                     {services.map((service, index) => (
                         <div key={index} className={`menu-item ${service.style}`}>
-                            <img src={service.img} style={{ width: '90px', height: '70px' }} />
-
+                            <img src={service.img} style={{ width: '90px', height: '70px' }} alt="Serviço" />
                         </div>
-
-
                     ))}
-                    <div className="ver-tudo"><a href="">Ver tudo</a></div>
+                    <div className="ver-tudo"><a href="#">Ver tudo</a></div>
                 </div>
 
-                <h1 className='frase-homepage'>Paixão por colocar os pacientes em </h1>
-                <h1 className='frase-homepage'>primeiro lugar.</h1>
+                <h1 className='frase-homepage'>Paixão por colocar os pacientes em primeiro lugar.</h1>
 
                 <div className='lista-homepage'>
                     <ul>
                         <li>Uma paixão pela cura</li>
-                        <li>Uma legado de exelencia</li>
-                        <li>Uma legado de exelencia</li>
+                        <li>Um legado de excelência</li>
+                        <li>Profissionais capacitados</li>
                     </ul>
                     <ul>
-                        <li>Uma paixão pela cura</li>
-                        <li>Uma legado de exelencia</li>
-                        <li>Uma legado de exelencia</li>
+                        <li>Ambiente seguro e moderno</li>
+                        <li>Atendimento humanizado</li>
+                        <li>Tecnologia de ponta</li>
                     </ul>
                 </div>
 
                 <div className='imgLateral'>
-                    <img src="../src/img/grupo.png" alt="" />
-                    <img src="../src/img/cuidados.png" alt="" />
-
+                    <img src={grupo} alt="Grupo" />
+                    <img src={cuidados} alt="Cuidados" />
                 </div>
 
                 <div className='segundoParagrafo'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Quisque placerat scelerisque tortor ornare ornare. Quisque placerat scelerisque tortor ornare<br /> ornare  Convallis felis vitae tortor augue. Velit nascetur proin massa in. <br /> Consequat faucibus porttitor enim et.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat scelerisque.<br/> Convallis felis vitae tortor augue. Velit nascetur proin massa in.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat scelerisque tortor ornare ornare.</p>
+                    <p>Convallis felis vitae tortor augue. Velit nascetur proin massa in. Consequat faucibus porttitor enim et.</p>
                 </div>
-                
+
                 <div className='subtitulosHome'>
-                <p className='titulo'>SEMPRE CUIDANDO</p>
-                <h4 className='segundoTitulo'>Nossas Especialidades</h4>
+                    <p className='titulo'>SEMPRE CUIDANDO</p>
+                    <h4 className='segundoTitulo'>Nossas Especialidades</h4>
+                </div>
+
+
+                <div className="especialidades-grid">
+                    {especialidades.map((item, index) => (
+                        <div
+                            key={index}
+                            className={`especialidade ${item.nome === "Ortopedia" ? "ortopedia" : ""}`}
+                        >
+                            <img src={item.img} />
+                        </div>
+                    ))}
                 </div>
             </div>
+
             <Footer />
-
         </>
-
     );
 };
 
