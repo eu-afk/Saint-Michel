f135b5f7e3df4fd9944100de3591fd642a905f70
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-06 14:38:25 -0300

    criando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 
 import './index.css'
-import Footer from "./components/Footer/Footer"
-import NavBar from "./components/Navbar/NavBar"
 import HomePage from "../src/Pages/Home/HomePage"
 import './index.css'
-import Login from "./Pages/Login/LoginPage"
-import SobrePage from "./Pages/Sobre/SobrePage"
-import Servicos from "./Pages/Serviços/Servicos"
-import Medico from './Pages/Medicos/Medico'
-import Doutores from './components/Doutores/Doutores'
 
 function App() {
 
   return (
     <>
-      {/* <Medico/> */}
-      <Servicos />
-      {/* <SobrePage /> */}
-      {/* <HomePage /> */}
+      <HomePage />
     </>
   )
 }
