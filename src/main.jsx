cf9a96bd1132709d5f2109aad17ee5cbb0c6744a
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-06 15:30:28 -0300

    tirando update

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import App from './App.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
