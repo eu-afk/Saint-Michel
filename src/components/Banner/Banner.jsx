0b39eaff2a6fbcd3cf3f254ed49b6e12dfa06c54
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-07 00:21:51 -0300

    colocando imagem banner-frase

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,17 +3,8 @@ import './banner.css'
 function Banner() {
     return (
         <>
-            <img src="../src/img/Banner-Baixo-Sobre.png" alt="" className='Banner-Baixo-Sobre' />
-            <div className="container-text">
-                <h1 id='aspas'>"</h1>
-                <p className='texto-banner-baixo-sobre'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat scelerisque tortor ornare ornare. Quisque placerat scelerisque felis vitae tortor augue. Velit nascetur Consequat faucibus porttitor enim et.</p>
-                <div className="linhaTop">
-                    <hr id="linha" />
-                </div>
-                <p className='texto-banner-baixo-sobre'>John Doe</p>
-            </div>
+            <img src="../src/img/Banner-Frase.png" alt="" className='Banner-Baixo-Sobre' />
         </>
-
     )
 }
 
