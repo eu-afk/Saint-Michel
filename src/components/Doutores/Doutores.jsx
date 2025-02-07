f3571791c6bc10e92368a9337dbac29bfce1c090
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-07 00:31:13 -0300

    atualizações em banner

--- src/components/Doutores/Doutores.jsx
+++ src/components/Doutores/Doutores.jsx
@@ -14,31 +14,35 @@ const doctors = [
 
 const DoctorCard = ({ name, specialty, image }) => {
   return (
-    <div className=".card-doutores ">
-      <img src={image} alt={name} className="doctor-image" />
-      <div className="card-info-doutores">
-        <div className="texto-doutores">
-          <h3 className="card-info-h3">{name}</h3>
-          <p className="card-info-p"><b>{specialty}</b></p>
+    <>
+      <div>
+        <img src={image} alt={name} className="doctor-image" />
+        <div className="card-info-doutores">
+          <div className="texto-doutores">
+            <h3 className="card-info-h3">{name}</h3>
+            <p className="card-info-p"><b>{specialty}</b></p>
+          </div>
+          <div className="social-icons">
+            <i className="fab fa-facebook"><img src="src/img/icons8-facebook-novo-30.png" alt="" /></i>
+            <i className="fab fa-instagram"><img src="src/img/icons8-instagram-30.png" alt="" /></i>
+            <i className="fab fa-linkedin"><img src="src/img/icons8-linkedin-30 (1).png" alt="" /></i>
+          </div>
+          <button className="profile-button-doutores">Visualizar Perfil</button>
         </div>
-        <div className="social-icons">
-          <i className="fab fa-facebook"><img src="src/img/icons8-facebook-novo-30.png" alt="" /></i>
-          <i className="fab fa-instagram"><img src="src/img/icons8-instagram-30.png" alt="" /></i>
-          <i className="fab fa-linkedin"><img src="src/img/icons8-linkedin-30 (1).png" alt="" /></i>
-        </div>
-        <button className="profile-button-doutores">Visualizar Perfil</button>
       </div>
-    </div>
+    </>
   );
 };
 
 const Medico = () => {
   return (
     <>
-      <div className="container-doutores">
-        {doctors.map((doctor, index) => (
-          <DoctorCard key={index} {...doctor} />
-        ))}
+      <div className="card-doutores">
+        <div className="container-doutores">
+          {doctors.map((doctor, index) => (
+            <DoctorCard key={index} {...doctor} />
+          ))}
+        </div>
       </div>
     </>
   );
