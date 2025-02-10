44fea1f976a40402051b5c1b6cb95a1e61a0f4e6
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-10 07:08:49 -0300

    colocando useState em CadastroPage

--- src/Pages/Cadastro/CadastroPage.jsx
+++ src/Pages/Cadastro/CadastroPage.jsx
@@ -11,35 +11,40 @@ function CadastroPage() {
         email: '',
         senha: '',
         cpf: '',
-        endereço: '',
-        sexo: ''
+        endereco: ''
     });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
+
         setFormData({
             ...formData,
-            [name]: value
+            [name]: name === 'cpf' ? value.replace(/\D/g, '') : value, // Garantir que CPF seja string
         });
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        // Verificar se todos os campos estão preenchidos
+        if (!formData.nome || !formData.idade || !formData.email || !formData.senha || !formData.cpf || !formData.endereco) {
+            console.error('Todos os campos devem ser preenchidos.');
+            return;
+        }
+
         try {
-            const response = await axios.post('http://localhost:5000/api/pacientes', formData);
+            const response = await axios.post('http://localhost:3000/api/usuarios/', formData);
             console.log('Paciente cadastrado com sucesso:', response.data);
-            // Limpar o formulário após o envio
             setFormData({
                 nome: '',
                 idade: '',
                 email: '',
                 senha: '',
                 cpf: '',
-                endereço: '',
-                sexo: ''
+                endereco: ''
             });
         } catch (error) {
-            console.error('Erro ao cadastrar paciente:', error);
+            console.error('Erro ao cadastrar paciente:', error.message);
         }
     };
 
@@ -53,14 +58,14 @@ function CadastroPage() {
                     <input placeholder="Idade" name='idade' type='number' value={formData.idade} onChange={handleChange} />
                     <input placeholder="Email" name='email' type='email' value={formData.email} onChange={handleChange} />
                     <input placeholder="Senha" name='senha' type='password' value={formData.senha} onChange={handleChange} />
-                    <input placeholder="CPF" name='cpf' type='number' value={formData.cpf} onChange={handleChange} />
-                    <input placeholder="Endereço" name='endereço' type='text' value={formData.endereço} onChange={handleChange} />
-                    <select name="sexo" value={formData.sexo} onChange={handleChange}>
+                    <input placeholder="CPF" name='cpf' type='text' value={formData.cpf} onChange={handleChange} />
+                    <input placeholder="Endereço" name='endereco' type='text' value={formData.endereco} onChange={handleChange} />
+                    {/* <select name="sexo" value={formData.sexo} onChange={handleChange}>
                         <option value="">Sexo</option>
                         <option value="Masculino">Masculino</option>
                         <option value="Feminino">Feminino</option>
                         <option value="Outros">Prefiro não opinar</option>
-                    </select>
+                    </select> */}
                     <button type='submit'>Cadastrar</button>
                 </form>
             </div>
