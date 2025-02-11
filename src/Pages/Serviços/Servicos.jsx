1a57914295daf5401f2c36f7bd8844ca9ae760b9
Felipe Paulino <154370191+oFelipeee@users.noreply.github.com>
2025-02-11 16:36:35 -0300

    Fazendo página de serviços

--- src/Pages/Serviços/Servicos.jsx
+++ src/Pages/Serviços/Servicos.jsx
@@ -5,43 +5,65 @@ import Footer from "../../components/Footer/Footer";
 import Contato from "../../components/Contato/Contato";
 
 const cards = [
-  { id: 1, title: "Checkup", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 2, title: "Proctologista", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 3, title: "Endocrinologista", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 4, title: "Fonoaudióloga", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 5, title: "Pediatria", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-  { id: 6, title: "Oftalmologista", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." }
+  { id: 1, title: "Clínico Geral", description: "O serviço de Clínico Geral é a porta de entrada para o cuidado com a sua saúde. Nossos profissionais estão preparados para realizar atendimentos abrangentes, oferecendo diagnósticos precisos e orientações sobre os mais diversos problemas de saúde. O clínico geral avalia sintomas, solicita exames quando necessário e acompanha o tratamento de condições comuns, como gripes, dores, alergias, hipertensão, diabetes, entre outras. Além disso, ele atua na prevenção de doenças, promovendo check-ups regulares e aconselhamento sobre hábitos saudáveis.  Agende sua consulta e cuide da sua saúde com quem entende do assunto!", 
+    
+   },
 ];
 
 const ContactInfo = [
   { title: "EMERGÊNCIA", info: "(11) 6918-1525", img: "../src/img/icons8-siren-96.png" },
-  { title: "LOCALIZAÇÃO", info: "Av. Manoel da Silva, 3400", img: "../src/img/LocationIcon.png" },
-  { title: "E-MAIL", info: "saintmichiel@gmail.com", img: "../src/img/emailicon.png" },
-  { title: "HORÁRIO DE TRABALHO", info: "Dom-Dom 09:00-20:00", img: "../src/img/icons8-clock-50.png" }
 ];
 
 export default function HealthPage() {
   return (
     <>
       <Navbar />
+
       <img src="../src/img/logo servico.png" className="img-servicos" alt="Logo Servicos" />
-      <div className="container-servicos">
-        <div className="cards-section-servicos">
+      <br /><br />
+      <h1 className="tittle-contato">Clínico Geral</h1>
+      <h1 className="team-title">Principais serviços da nossa rede</h1>
+
+      {/* Seção com os cards lado a lado */}
+      <div className="team-section">
+        <div className="image-container">
           {cards.map((card) => (
             <div key={card.id} className="card-servico">
-              <div className="image-servico">
-                <img src={"../src/img/cards.png"} alt={card.title} className="imgCard-servico" />
+              <div className="image-servico" style={{ flex: 1.2 }}>
+                <img src={"src/img/ClinicoGeral.jpg"} alt={card.title} className="imgCard-servico" />
               </div>
               <div className="content-servico">
-                <h3>{card.title}</h3>
-                <p>{card.description}</p>
-                <a href="#">Leia mais →</a>
+                <h3 className="title-card">{card.title}</h3>
+                <p className="card-description">{card.description}</p>
+                <br />
+                <button className="team-button">Saiba Mais</button>
               </div>
             </div>
           ))}
         </div>
       </div>
-      <Contato/>
+
+
+      <div className="team-section">
+        <div className="image-container">
+          {cards.map((card) => (
+            <div key={card.id} className="card-servico">
+              <div className="image-servico" style={{ flex: 1.2 }}>
+                <img src={"src/img/ClinicoGeral.jpg"} alt={card.title} className="imgCard-servico" />
+              </div>
+              <div className="content-servico">
+                <h3 className="title-card">{card.title}</h3>
+                <p className="card-description">{card.description}</p>
+                <br />
+                <button className="team-button">Saiba Mais</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      
+
+      <Contato />
       <Footer />
     </>
   );
