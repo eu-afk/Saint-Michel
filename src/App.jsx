208b63dcfce1537bf5b8c2bd00228fe1a9ac6825
Victor dos Santos <victorsantos.goal@gmail.com>
2025-02-11 16:39:44 -0300

    arrumando detalhes de homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import './index.css'
 import HomePage from "../src/Pages/Home/HomePage"
-import './index.css'
 
 
 function App() {
--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './HomePage.css';
 
 import medicos from '../../img/medicos.png';
+import medicosFelizes from '../../img/medicos-felizes.png';
+import cuidadoSaudeDoMedico from '../../img/cuidados-saude-do-medico.png';
+import casalDeMedicos from '../../img/casal de medicos.png';
 import Navbar from '../../components/Navbar/NavBar';
 import Footer from '../../components/Footer/Footer';
 import checkup from '../../img/checkup.png';
@@ -14,8 +17,8 @@ import grupo from '../../img/grupo.png';
 import cuidados from '../../img/cuidados.png';
 import CarouselMedico from '../../components/CarrosselMedicos/CarrosselMedicos'
 
-import neurologia from '../../img/neurologia.png';
 import coracaoEscuro from '../../img/coracaoEscuro.png';
+/* import neurologia from '../../img/neurologia.png';
 import oncologia from '../../img/oncologia.png';
 import otorrino from '../../img/otorrino.png';
 import oftalmologia from '../../img/oftalmologia.png';
@@ -25,7 +28,7 @@ import renal from '../../img/renal.png';
 import gastro from '../../img/gastro.png';
 import urologia from '../../img/urologia.png';
 import dermatologia from '../../img/dermatologia.png';
-import ginecologia from '../../img/ginecologia.png';
+import ginecologia from '../../img/ginecologia.png'; */
 import { Link } from 'react-router-dom';
 import Contato from '../../components/Contato/Contato'
 
@@ -33,23 +36,23 @@ import Contato from '../../components/Contato/Contato'
 const services = [
     { img: checkup },
     { img: cardiograma },
-    { img: dna},
+    { img: dna },
     { img: bancoSangue }
 ];
 
 const especialidades = [
-    { img: neurologia },
     { nome: "Ortopedia", img: coracaoEscuro },
-    { img: oncologia },
-    { img: otorrino },
-    { img: oftalmologia },
-    { img: cardiologia },
-    { img: pneumologia },
-    { img: renal },
-    { img: gastro },
-    { img: urologia },
-    { img: dermatologia },
-    { img: ginecologia }
+    { nome: "Ortopedia", img: coracaoEscuro },
+    { nome: "Oncologia", img: coracaoEscuro },
+    { nome: "Otorrino", img: coracaoEscuro },
+    { nome: "Oftalmologia", img: coracaoEscuro },
+    { nome: "Cardiologia", img: coracaoEscuro },
+    { nome: "Pneumologia", img: coracaoEscuro },
+    { nome: "Renal", img: coracaoEscuro },
+    { nome: "Gastro", img: coracaoEscuro },
+    { nome: "Urologia", img: coracaoEscuro },
+    { nome: "dermatologia", img: coracaoEscuro },
+    { nome: "Ginecologia", img: coracaoEscuro }
 ];
 
 const HomePage = () => {
@@ -61,22 +64,23 @@ const HomePage = () => {
             <div className='carrossel'>
                 <Carousel>
                     <Carousel.Item>
-                        <img className='imagem' src={medicos} alt="Imagem um" />
+                        <img className='imagem' src={medicosFelizes} alt="Imagem dois" />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img className='imagem' src={medicos} alt="Imagem dois" />
+                        <img className='imagem' src={cuidadoSaudeDoMedico} alt="Imagem dois" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className='imagem' src={casalDeMedicos} alt="Imagem dois" />
                     </Carousel.Item>
                 </Carousel>
             </div>
-
-
 
             {/* Começo da página */}
             <div className="homepage-container">
                 <p className='titulo'>BEM VINDO A SAINT-MICHEL</p>
                 <h4 className='segundoTitulo'>Um ótimo lugar para receber cuidados</h4>
-                <p className='paragrafoHome'>Confira os serviços fornecidos pela nossa rede.</p>
-                <p className='saibaMais'> <Link to='/servicos'>Saiba mais</Link>
+                <p className='paragrafoHome'>Confira a história do nosso hospital.</p>
+                <p className='saibaMais'> <Link to='/sobre'>Saiba mais</Link>
                     <img className='seta' src="../src/img/seta.png" />
                 </p>
 
@@ -95,18 +99,16 @@ const HomePage = () => {
                 {/* Serviços */}
                 <div className="menu-servicos-homepage">
                     {services.map((service, index) => (
-                        <div key={index} className={`menu-item-homepage ${service.style}`}>
-                            <img src={service.img} style={{ width: '90px', height: '70px' }} alt="Serviço" />
+                        <div key={index} className={'menu-item-homepage'}>
+                            <img src={service.img} alt="Serviço" className='img-menu-servicos'/>
                         </div>))}
-                    <div className="ver-tudo"><a href="#">Ver tudo</a></div>
+                    <div className="ver-tudo"><Link to='/servicos'>Ver tudo</Link></div>
                 </div>
 
 
                 <div className=''>
                     {/*  Frase Principal */}
-                    <br />
-                    <h1 className='frase-homepage'>Paixão por colocar os pacientes em primeiro lugar.</h1>
-
+                    <h1 className='frase-homepage'><b>Paixão por colocar os pacientes em primeiro lugar.</b></h1>
                     {/* Lista de Serviços */}
                     <div className='lista-homepage'>
                         <ul>
@@ -122,7 +124,6 @@ const HomePage = () => {
                     </div>
                     {/* Segundo Paragrafo */}
                     <div className='segundoParagrafoHome'>
-                        <br></br>
                         <p>Na Rede Hospitalar Saint Michel, temos uma paixão pela cura que guia cada passo do nosso trabalho, sustentada por um legado de excelência em cuidados médicos. Contamos com profissionais capacitados e dedicados, prontos para oferecer o melhor atendimento em um ambiente seguro e moderno. Nossa prioridade é o atendimento humanizado, colocando o paciente no centro de tudo, enquanto utilizamos tecnologia de ponta para diagnósticos precisos e tratamentos eficazes. Aqui, sua saúde está em boas mãos..</p>
                     </div>
                 </div>
@@ -163,10 +164,10 @@ const HomePage = () => {
 
             {/* Segunda Imagem*/}
             <img className='medicoBranco' src="../src/img/medicoBranco.png" />
-            
-            
-                {/* Titulos finais */}
-              <div className='titulosFinais'>
+
+
+            {/* Titulos finais */}
+            <div className='titulosFinais'>
                 <p className='titulo'>CUIDADO CONFIÁVEL</p>
                 <h4 className='segundoTitulo'>Nossos Médicos</h4>
             </div>
